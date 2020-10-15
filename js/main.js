88b72b4add7d307161a2b8169e6533c2dabbc541
Agustin Ligustri <agus.ligustri@hotmail.com>
2020-10-15 19:47:52 -0300

    Terminado de bicicletas.html con Gallery

--- js/main.js
+++ js/main.js
@@ -84,4 +84,24 @@ $('.owl2').owlCarousel({
 	}
 })
 
+/**Gallery */
+$('.gallery-menu ul li').click(function(){
+	$('.gallery-menu ul li').removeClass('active');
+	$(this).addClass('active');
+	
+	var selector = $(this).attr('data-filter');
+	$('.gallery-item').isotope({
+		filter:selector
+	});
+	return  false;
+});
 
+$(document).ready(function() {
+var popup_btn = $('.popup-btn');
+popup_btn.magnificPopup({
+type : 'image',
+gallery : {
+	enabled : true
+}
+});
+});
